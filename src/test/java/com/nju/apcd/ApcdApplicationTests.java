6ab9419240b315faa8df3c77c5049acf5092c2b2
endeavor2299 <1932317892.@qq.com>
2022-12-24 23:20:17 +0800

    init project

---
+++ src/test/java/com/nju/apcd/ApcdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nju.apcd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApcdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
